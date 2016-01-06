a5390b0bbf32c410b0dadd332162cd52c81f49f4
Nano <ymuruvi@gmail.com>
2016-01-06 17:14:08 -0500

    Fixed Obstacle value errors.

--- core/src/com/nnldev/fartlek/sprites/Obstacle.java
+++ core/src/com/nnldev/fartlek/sprites/Obstacle.java
@@ -6,61 +6,78 @@ import com.badlogic.gdx.math.Vector3;
 
 
 public class Obstacle {
-        private Vector3 position;
-	private static Texture texture;
-	private Rectangle rectangle;
-	private static String path; //name of obstacle image, used to compare and make sure there aren't duplicate obstacles
-	private static boolean empty; //tells whether or not the obstacle is empty
-	public static int obstacleSpeed = -2;
-	
-	/* constructor
-	path, name of texture
-	x, x position
-	y, pretty much just to animate it
-	*/
-	public Obstacle(String pathh, float xx, float yy,boolean e) {//i had to use weird double letters to avoid errors with this.
-		path = pathh;
-		texture = new Texture(path); //there can be multiple textures for obstacles
-		position.x=xx;
-		position.y=yy;
-		rectangle = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
-		empty = e;
-	}
-	//returns status of box, "empty" or not
-	public boolean emptyStatus(){
-		return empty;
-	}
-	//sets status of box
-	public void setEmpty(boolean e){
-		empty = e;
-	}
-	//set texture of box
-	public void setTexture(String p){
-		path = p;
-		texture = new Texture(p);
-	}
-	public void setY(float y){
-		position.y = yy;
-	}
-	public int getY(){
-		return position.y;
-	}
-	public int getX(){
-		return position.x;
-	}
-	public void setX(float x){
-		position.x=xx;
-	}
-	public Vector3 getPosition(){
-		return position;
-	}
-	public void setPosition(float pos){
-		position.set(pos);
-	}
-	/*public static String toString(){
-		return "X Value: "+ position.x +"Texture: "+ path;
-	}*/
-	public Texture getTexture(){
-		return texture;
-	}
+    private Vector3 position;
+    private static Texture texture;
+    private Rectangle rectangle;
+    private static String path; //name of obstacle image, used to compare and make sure there aren't duplicate obstacles
+    private static boolean empty; //tells whether or not the obstacle is empty
+    public static int obstacleSpeed = -2;
+
+    /** constructor
+    path, name of texture
+    x, x position
+    y, pretty much just to animate it
+    */
+    public Obstacle(String pathh, float x, float y, boolean e) {//i had to use weird double letters to avoid errors with this.
+        position = new Vector3();
+        path = pathh;
+        texture = new Texture(path); //there can be multiple textures for obstacles
+        position.x = x;
+        position.y = y;
+        rectangle = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+        empty = e;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean emptyStatus() {
+        return empty;
+    }
+
+    /**
+     *
+     * @param e
+     */
+    public void setEmpty(boolean e) {
+        empty = e;
+    }
+
+    /**
+     *
+     * @param p
+     */
+    public void setTexture(String p) {
+        path = p;
+        texture = new Texture(p);
+    }
+
+    public void setY(float y) {
+        position.y = y;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public float getX() {
+        return position.x;
+    }
+
+    public void setX(float x) {
+        position.x = x;
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3 position) {
+        this.position.set(position);
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
 }
