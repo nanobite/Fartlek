e31f410a2de79b4d57a238523660648fd15a7c2e
Santiago <NiXie6096@student.UGDSB.ED>
2016-01-06 14:57:08 -0500

    Modified obstacles.

--- core/src/com/nnldev/fartlek/sprites/Obstacle.java
+++ core/src/com/nnldev/fartlek/sprites/Obstacle.java
@@ -6,9 +6,8 @@ import com.badlogic.gdx.math.Vector3;
 
 
 public class Obstacle {
+        private Vector3 position;
 	private static Texture texture;
-	private static int x;
-	private static int y;
 	private Rectangle rectangle;
 	private static String path; //name of obstacle image, used to compare and make sure there aren't duplicate obstacles
 	private static boolean empty; //tells whether or not the obstacle is empty
@@ -19,11 +18,11 @@ public class Obstacle {
 	x, x position
 	y, pretty much just to animate it
 	*/
-	public Obstacle(String pathh, int xx, int yy,boolean e) {//i had to use weird double letters to avoid errors with this.
+	public Obstacle(String pathh, float xx, float yy,boolean e) {//i had to use weird double letters to avoid errors with this.
 		path = pathh;
 		texture = new Texture(path); //there can be multiple textures for obstacles
-		x=xx;
-		y=yy;
+		position.x=xx;
+		position.y=yy;
 		rectangle = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
 		empty = e;
 	}
@@ -40,20 +39,26 @@ public class Obstacle {
 		path = p;
 		texture = new Texture(p);
 	}
-	public void setY(int yy){
-		y = yy;
+	public void setY(float y){
+		position.y = yy;
 	}
 	public int getY(){
-		return y;
+		return position.y;
 	}
 	public int getX(){
-		return x;
+		return position.x;
 	}
-	public void setX(int xx){
-		x=xx;
+	public void setX(float x){
+		position.x=xx;
+	}
+	public Vector3 getPosition(){
+		return position;
+	}
+	public void setPosition(float pos){
+		position.set(pos);
 	}
 	/*public static String toString(){
-		return "X Value: "+ x +"Texture: "+ path;
+		return "X Value: "+ position.x +"Texture: "+ path;
 	}*/
 	public Texture getTexture(){
 		return texture;
